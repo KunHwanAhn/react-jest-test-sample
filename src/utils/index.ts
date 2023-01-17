2a00cdc5e4fafbf000577eafcdd5259d8bb67b22
Conan Kunhwan Ahn <kunhwon@gmail.com>
2023-01-17 17:27:42 +0900

    테스트 환경 구축 (#1)

---
+++ src/utils/index.ts
@@ -0,0 +1,5 @@
+type Sum = (a: number, b: number) => number;
+const sum: Sum = (a, b) => a + b;
+
+// eslint-disable-next-line import/prefer-default-export
+export { sum };
--- src/views/main/Index.tsx
+++ src/views/main/Index.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
+import { sum } from '@/utils';
+
 export default function Index() {
   return (
-    <div>Hello world!</div>
+    <>
+      <div>Hello World</div>
+      <div>{sum(1, 2)}</div>
+    </>
   );
 }
