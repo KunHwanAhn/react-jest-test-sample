5f2b7cc0ab2d98bcddefe53de02a49e4f6201ec9
Conan Kunhwan Ahn <kunhwon@gmail.com>
2023-01-17 17:47:44 +0900

    웹팩 관련 설정 추가 (#2)

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,16 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  moduleDirectories: ['node_modules'],
   moduleNameMapper: {
+    // https://jestjs.io/docs/webpack#configuring-jest-to-find-our-files - Webpack Alias
     '@/(.*)$': '<rootDir>/src/$1',
+    // https://jestjs.io/docs/webpack#handling-static-assets - Webpack - Handling Static Assets
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
   },
   transform: {
-    '^.\\.tsx?$': 'ts-jest',
+    '\\.[jt]sx?$': 'babel-jest',
   },
   transformIgnorePatterns: [
     '<rootDir>/node_modules/',
